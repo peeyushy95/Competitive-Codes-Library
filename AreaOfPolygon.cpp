f922b69592ce52a5533492d526270c1881bc59cc
Peeyush Yadav <peeyushy95@gmail.com>
2015-06-15 19:07:34 +0530

    Area of n-sided polygon (non-self intersecting)
    Vertices should be in Clockwise or anticlockwise order.

---
+++ AreaOfPolygon.cpp
@@ -0,0 +1,26 @@
+
+
+// Area of polygon = ans/2
+// Convert into double if exact area is required
+
+long long getArea(){
+    int m;
+    scanf("%d",&m);
+    
+	vector< pair<long long,long long> > vertices;
+    vertices.resize(m+1);
+    
+	for(int i=0;i<m;i++)
+        scanf("%lld %lld",&vertices[i].first,&vertices[i].second);
+    
+	vertices[m].first=vertices[0].first;
+    vertices[m].second = vertices[0].second;
+    
+	long long ans =0;
+    for(int i=0;i<m;i++)
+            ans+= vertices[i].first * vertices[i+1].second - vertices[i+1].first * vertices[i].second;
+	
+
+    return abs(ans);
+}
+

3cacaab3a954350b29825df355d8202f7b54feda
Peeyush Yadav <peeyushy95@gmail.com>
2015-06-17 19:02:58 +0530

    Non-self intersecting and vertices should be in clockwise or anticlockwise order

--- AreaOfPolygon.cpp
+++ AreaOfPolygon.cpp
@@ -7,16 +7,16 @@ long long getArea(){
     int m;
     scanf("%d",&m);
     
-	vector< pair<long long,long long> > vertices;
+    vector< pair<long long,long long> > vertices;
     vertices.resize(m+1);
     
-	for(int i=0;i<m;i++)
-        scanf("%lld %lld",&vertices[i].first,&vertices[i].second);
+    for(int i=0;i<m;i++)
+     		scanf("%lld %lld",&vertices[i].first,&vertices[i].second);
     
-	vertices[m].first=vertices[0].first;
+    vertices[m].first=vertices[0].first;
     vertices[m].second = vertices[0].second;
     
-	long long ans =0;
+    long long ans =0;
     for(int i=0;i<m;i++)
             ans+= vertices[i].first * vertices[i+1].second - vertices[i+1].first * vertices[i].second;
 	

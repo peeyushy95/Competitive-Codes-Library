a2b989dcff7d0340c5e0278ebc3f09bc935155e2
Peeyush Yadav <peeyushy95@gmail.com>
2015-08-24 21:35:49 +0530

    Euler-Totient

---
+++ Euler-Totient.cpp
@@ -0,0 +1,25 @@
+
+method :1
+
+	int phi (int n) {
+		int result = n;
+		for (int i=2; i*i<=n; ++i)
+			if (n % i == 0) {
+				while (n % i == 0)
+					n /= i;
+				result -= result / i;
+			}
+		if (n > 1)
+			result -= result / n;
+		return result;
+	}
+
+...............................................................................
+method :2
+
+
+	phi[0]  = 0;
+	for (int i=1; i<maxn; ++i) phi[i] = i - 1;
+	for (int i=1; i<maxn; ++i)
+		for (int j=i+i; j<maxn; j+=i)
+		         phi[j] -= phi[i];
